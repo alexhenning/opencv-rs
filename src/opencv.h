63e9625f1a04e264e22c27ff4fe1f1a00b08395a
Alex Henning <elcron@gmail.com>
2014-11-16 15:57:57 -0500

    Added submodules the right way.

--- src/opencv.h
+++ src/opencv.h
@@ -7,3 +7,12 @@
 /* #include <opencv/cxmisc.h> */
 #include <opencv/cvwimage.h>
 #include <opencv/highgui.h>
+
+/* #include <opencv/cv.hpp> */
+/* #include <opencv/ml.h> */
+
+/* /\* #include <opencv/cvaux.h> *\/ */
+/* #include <opencv/cxcore.hpp> */
+/* /\* #include <opencv/cxmisc.h> *\/ */
+/* #include <opencv/cvwimage.h> */
+/* #include <opencv/highgui.h> */
